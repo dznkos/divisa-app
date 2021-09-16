74792176f5ef6d0db2c92c4397caf9facb7980bc
Luis Vice <khanccer@gmail.com>
2021-09-16 08:13:51 -0500

    divisa v0.2

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvertidorComponent implements OnInit {
 
+  cantidad = 0;
+  dedivisa = 'USD';
+  paradivisa = 'EUR';
+  total = 0;
+
+  monedas: string[] = ['USD', 'EUR','PEN']
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiardivisa() {
+
+    switch(this.dedivisa){
+
+      case 'USD':
+        if (this.paradivisa === 'USD') {
+          this.total = this.cantidad;
+        } 
+        if ( this.paradivisa === 'EUR')
+        {
+          this.total = Math.round(this.cantidad * 0.85);
+        }
+        if ( this.paradivisa === 'PEN')
+        {
+          this.total = Math.round(this.cantidad * 4.11);
+        }
+        break;
+      case 'EUR':
+        if (this.paradivisa === 'USD') {
+          this.total = Math.round(this.cantidad * 1.18);
+        } 
+        if ( this.paradivisa === 'EUR')
+        {
+          this.total = this.cantidad;
+        }
+        if ( this.paradivisa === 'PEN')
+        {
+          this.total = Math.round(this.cantidad * 4.86);
+        }
+        break; 
+      case 'PEN':
+        if (this.paradivisa === 'USD') {
+          this.total = Math.round(this.cantidad * 0.24);
+        } 
+        if ( this.paradivisa === 'EUR')
+        {
+          this.total = Math.round(this.cantidad * 0.21);
+        }
+        if ( this.paradivisa === 'PEN')
+        {
+          this.total = this.cantidad;
+        }
+        break;
+    }
+
   }
 
 }
